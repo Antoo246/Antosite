41fb3f602e10b3f082ed6b3fc5b861077e58a912
Anto426 <amanocchio426@gmail.com>
2024-09-23 20:04:15 +0200

    Update site styling and content
    - Update site title and favicon
    - Update Bootstrap and Bootstrapicon versions
    - Update custom CSS for background gradient animation
    - Update About Me section text
    - Update social media links
    - Remove unused JavaScript function
    - Add JavaScript function for text manipulation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
-function changeBackground() {
-    document.body.style.backgroundColor = "red";
+const rootStyles = getComputedStyle(document.documentElement);
+
+const layoutroot = document.getElementById('layoutroot');
+
+function textchenger(text, idtext) {
+    let char = text.split('');
+    for (let i = 0; i < char.length; i++) {
+        
+        char[i] = char[i].charCodeAt(0) + 1;
+    }
 }
+
