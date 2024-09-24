da6a1f42c875b4cd72495fee55fc3f08c28fa71b
アントネッロ <87477585+Anto426@users.noreply.github.com>
2024-09-24 12:55:24 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -26,3 +26,53 @@ function changertextinterval(text, idtext) {
         changetext(text, campo);
     }, 6000);
 }
+
+
+
+
+
+
+
+
+const rootStyles = getComputedStyle(document.documentElement);
+
+const layoutroot = document.getElementById('layoutroot');
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let textchenger = new Textchenger()
+    textchenger.changertextinterval("I'm a high school student who likes Information and Communications Technology 💻✨", 'aboutfild')
+});
+
+
+/*
+class Textchenger {
+
+    constructor() {
+        this.timesinglechar = 4
+    }
+
+    changetext(text, campo) {
+        return new Promise(async (resolve) => {
+            for (let i = 0; i < text.length; i++) {
+                await setTimeout(() => {
+                    campo.innerHTML += text[i];
+                }, this.timesinglechar)
+            }
+            resolve(0)
+        })
+    }
+
+    changertextinterval(text, idtext) {
+        let campo = document.getElementById(idtext);
+        this.changetext(text, campo)
+            .then(() => {
+                console.log(idtext)
+
+            })
+
+    }
+
+}
+
+*/
