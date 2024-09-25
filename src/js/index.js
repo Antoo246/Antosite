eda18cbfe8abefac9ba3d039db6d61d74e8d1921
Anto426 <amanocchio426@gmail.com>
2024-09-25 11:26:45 +0200

    Update site styling in index.html and update logo size in style.css

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let fild = document.getElementById("anto-About-fild");
+    textWrriter("I'm a high school student who likes programming 💻✨", fild);
+});
+
+
+//  Function for simulate a text writer
+function textWrriter(text, element) {
+    if (text.length > 0) {
+        let caracther = text.split("");
+        for (let i = 0; i < caracther.length; i++) {
+            setTimeout(() => {
+                element.innerHTML += `${caracther[i]}`;
+            }, 200* i);
+        }
+    } else {
+        console.log("Text is empty");
+    }
+}
