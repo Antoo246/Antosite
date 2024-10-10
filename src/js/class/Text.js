1a1f59317b293cc5ab6e246b3c54b1b915b625ef
Anto426 <amanocchio426@gmail.com>
2024-10-10 16:14:26 +0200

    Refactor setlenText function in Text.js

--- src/js/class/Text.js
+++ src/js/class/Text.js
@@ -18,27 +18,27 @@ class TextClass {
 
 
     // Function for calculate the width of a text
-// Function for calculating the width of a text
-setlenText(textFild, testo) {
-    const stile = window.getComputedStyle(textFild);
-    const elemento = document.createElement('span'); 
-    elemento.style.cssText = stile.cssText; 
+    // Function for calculating the width of a text
+    setlenText(textFild, testo) {
+        const stile = window.getComputedStyle(textFild);
+        const elemento = document.createElement('span');
+        elemento.style.cssText = stile.cssText;
 
 
-    elemento.style.visibility = 'hidden';
-    elemento.style.whiteSpace = 'nowrap';
-    elemento.style.position = 'absolute';
-    elemento.style.boxSizing = 'border-box';
+        elemento.style.visibility = 'hidden';
+        elemento.style.whiteSpace = 'nowrap';
+        elemento.style.position = 'absolute';
+        elemento.style.boxSizing = 'border-box';
 
-    elemento.textContent = testo; 
-    document.body.appendChild(elemento);
-    const len = elemento.offsetWidth; 
-    document.body.removeChild(elemento);
+        elemento.textContent = testo;
+        document.body.appendChild(elemento);
+        const len = elemento.offsetWidth;
+        document.body.removeChild(elemento);
 
-    // Applica la larghezza calcolata all'elemento target
-    textFild.style.width = len + 'px'; 
-    console.log("calculated length ", len + 'px');
-}
+        // Applica la larghezza calcolata all'elemento target
+        textFild.style.width = len + 'px';
+        console.log("calculated length ", len + 'px');
+    }
 
 
 
