67054794b2db763c9c9d4c68a4a2b2062a33d479
anto426 <amanocchio426@gmail.com>
2024-10-10 10:06:34 +0200

    Refactor index.html and style.css, optimize external resource loading

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,9 @@ const AntoAboutFild = "I'm a high school student who likes programming 💻✨"
 
 // Function for loadpage
 function Load() {
+    let prymarybox = document.getElementById("anto-prymarybox");
     let textFild = document.getElementById("anto-About-fild");
+    let loader = document.getElementById("anto-loader");
     Textd.setLarghezzaTesto(textFild, AntoAboutFild);
 
     FetchDataIn.fetchGithubData(githubusername).then(data => {
@@ -25,7 +27,13 @@ function Load() {
         tag.innerHTML = data.login;
         DynamicColorIn.setImg(logo);
         DynamicColorIn.applyTheme();
-        Textd.textWrriter(AntoAboutFild, textFild);
+        setTimeout(() => {
+            loader.style.display = "none";
+            prymarybox.classList.add("fade-in");
+            prymarybox.style.display = "flex";
+            Textd.textWrriter(AntoAboutFild, textFild);
+        }, 1000);
+
 
     }).catch(error => {
         console.error(error);
